da2833c30fc7e795889d0460dd58c0e9137d50f3
MiguelAngelCastroEscamillaINSTRUCTOR <macastro40@misena.edu.co>
2022-11-15 10:58:06 -0500

    Archivos Base

---
+++ main.js
@@ -0,0 +1,22 @@
+addEventListener("DOMContentLoaded", (e)=>{
+   let ran = Math.random()*50;
+   console.log(Math.trunc(ran));
+   
+   
+   
+    // let num = 20.46412
+    // let quitar = Math.trunc(num);
+    // let redondear = Math.round(num);
+    // let atrasredondear = Math.fround(num);
+
+    // console.log(num);
+    // console.log(quitar);
+    // console.log(redondear);
+    // console.log(atrasredondear);
+
+
+
+    // let num = Number("200");
+    // let config = Intl.NumberFormat('de-DE',{style: 'currency', currency: 'COP', minimumFractionDigits:5});
+    // console.log(`$ ${config.format(num)}`);
+})
